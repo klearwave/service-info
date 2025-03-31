14b8448da0350a766b7fbd233d2fb493ef92b5e8
Dustin Scott <dustin.scott18@gmail.com>
2025-03-31 16:00:37 -0500

    feat: Fixes #1, add support for TLS certs
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- deploy/infra/sandbox/setup.tf
+++ deploy/infra/sandbox/setup.tf
@@ -54,7 +54,6 @@ data "http" "login" {
 
 locals {
   auth_token = regex("TOKEN=([^;]+)", data.http.login.response_headers["Set-Cookie"])[0]
-  csrf_token = data.http.login.response_headers["X-Csrf-Token"]
 }
 
 data "http" "current_site_config" {
@@ -94,104 +93,59 @@ resource "unifi_port_forward" "service_info" {
   fwd_ip   = var.backend_service_address
   fwd_port = var.backend_service_port
 
-  port_forward_interface = "wan2"
+  port_forward_interface = "wan"
 }
 
-resource "unifi_firewall_group" "local_usg_ports" {
-  site = local.site_name
+#
+# NOTE: firewall rule seems to be automatically created for port forward
+#       but here may be an example of how to do it manually if the above 
+#       fails for some reason.
+#
 
-  name    = "port_usg_klearwave_service_info"
-  type    = "port-group"
-  members = [tostring(var.usg_port), tostring(var.backend_service_port)]
-}
-
-resource "unifi_firewall_group" "local_usg_addresses" {
-  site = local.site_name
-
-  name    = "address_local_usg_klearwave_service_info"
-  type    = "address-group"
-  members = [var.public_ip, var.backend_service_address]
-}
-
-# resource "unifi_firewall_rule" "service_info" {
+# resource "unifi_firewall_group" "local_usg_ports" {
 #   site = local.site_name
-
-#   name       = "allow_klearwave_service_info_in_${var.usg_port}"
+#
+#   name    = "port_usg_klearwave_service_info"
+#   type    = "port-group"
+#   members = [tostring(var.usg_port), tostring(var.backend_service_port)]
+# }
+#
+# resource "unifi_firewall_group" "local_usg_addresses" {
+#   site = local.site_name
+#
+#   name    = "address_local_usg_klearwave_service_info"
+#   type    = "address-group"
+#   members = [var.public_ip, var.backend_service_address]
+# }
+#
+# resource "unifi_firewall_rule" "service_info_wan_local" {
+#   site = local.site_name
+#
+#   name       = "allow_klearwave_service_info_local_${var.usg_port}"
 #   action     = "accept"
 #   ruleset    = "WAN_LOCAL"
-#   rule_index = 2100
-
-#   protocol         = "tcp"
-#   src_address      = "0.0.0.0"
-#   src_port         = "32768-65535"
-#   src_network_type = "NETv4"
-#   dst_address      = var.public_ip
-#   dst_port         = tostring(var.usg_port)
-#   dst_network_type = "NETv4"
-
-#   enabled = true
+#   enabled    = true
+#   rule_index = 2001
+#   protocol   = "tcp"
+#
+#   dst_firewall_group_ids = [
+#     unifi_firewall_group.local_usg_addresses.id,
+#     unifi_firewall_group.local_usg_ports.id
+#   ]
 # }
-
-resource "unifi_firewall_rule" "service_info_wan_local" {
-  site = local.site_name
-
-  name       = "allow_klearwave_service_info_local_${var.usg_port}"
-  action     = "accept"
-  ruleset    = "WAN_LOCAL"
-  enabled    = true
-  rule_index = 2001
-  protocol   = "tcp"
-
-  dst_firewall_group_ids = [
-    unifi_firewall_group.local_usg_addresses.id,
-    unifi_firewall_group.local_usg_ports.id
-  ]
-}
-
-resource "unifi_firewall_rule" "service_info_wan_in" {
-  site = local.site_name
-
-  name       = "allow_klearwave_service_info_in_${var.usg_port}"
-  action     = "accept"
-  ruleset    = "WAN_IN"
-  enabled    = true
-  rule_index = 2002
-  protocol   = "tcp"
-
-  dst_firewall_group_ids = [
-    unifi_firewall_group.local_usg_addresses.id,
-    unifi_firewall_group.local_usg_ports.id
-  ]
-}
-
-resource "unifi_firewall_rule" "service_info_lan_local" {
-  site = local.site_name
-
-  name       = "allow_klearwave_service_info_local_${var.usg_port}"
-  action     = "accept"
-  ruleset    = "LAN_LOCAL"
-  enabled    = true
-  rule_index = 2003
-  protocol   = "tcp"
-
-  dst_firewall_group_ids = [
-    unifi_firewall_group.local_usg_addresses.id,
-    unifi_firewall_group.local_usg_ports.id
-  ]
-}
-
-resource "unifi_firewall_rule" "service_info_lan_in" {
-  site = local.site_name
-
-  name       = "allow_klearwave_service_info_in_${var.usg_port}"
-  action     = "accept"
-  ruleset    = "LAN_IN"
-  enabled    = true
-  rule_index = 2004
-  protocol   = "tcp"
-
-  dst_firewall_group_ids = [
-    unifi_firewall_group.local_usg_addresses.id,
-    unifi_firewall_group.local_usg_ports.id
-  ]
-}
+#
+# resource "unifi_firewall_rule" "service_info_wan_in" {
+#   site = local.site_name
+#
+#   name       = "allow_klearwave_service_info_in_${var.usg_port}"
+#   action     = "accept"
+#   ruleset    = "WAN_IN"
+#   enabled    = true
+#   rule_index = 2002
+#   protocol   = "tcp"
+#
+#   dst_firewall_group_ids = [
+#     unifi_firewall_group.local_usg_addresses.id,
+#     unifi_firewall_group.local_usg_ports.id
+#   ]
+# }
